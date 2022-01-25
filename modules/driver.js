5326e1fe266d8ee2549a8a35bc405360ca81922c
michael <mhendricks96@yahoo.com>
2022-01-24 23:05:51 -0800

    almost done with lab

---
+++ modules/driver.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const eventPool = require('../eventPool');
+
+eventPool.on('pickup', (order) => {
+  console.log('Driver picked up - ORDER_ID:', order.orderID, '\n');
+  eventPool.emit('in-transit', order);
+});
+
+function driverEvent(order){
+  console.log('Driver delivered');
+  console.log('ORDER_ID:', order.orderID, '\n');
+  //eventEmitter.emit('delivered', order);
+}
+
+module.exports = driverEvent;
---
+++ modules/vendor.js
@@ -0,0 +1,13 @@
+const eventPool = require('../eventPool.js');
+
+eventPool.on('delivered', (order) => {
+  console.log('VENDOR: Thank you,', order.customer, 'from,', order.store, '\n');
+});
+
+function vendorEvent(order){
+  console.log('VENDOR: start pickup');
+  console.log('ORDER_ID:', order.orderID, '\n');
+  eventPool.emit('pickup', order);
+}
+
+module.exports = vendorEvent;
